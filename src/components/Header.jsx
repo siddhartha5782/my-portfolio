9a6923b5123e1ffefe92fb84ddcfaf28ef05fd3e
Siddhartha <siddharthanalla@gmail.com>
2025-03-08 11:52:40 -0700

    updated nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,11 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-            <Nav.Link as={Link} to="/case-studies">Case Studies</Nav.Link>  {/* New Case Studies Tab */}
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/my-portfolio/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/my-portfolio/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/my-portfolio/projects">Projects</Nav.Link>
+            <Nav.Link as={Link} to="/my-portfolio/case-studies">Case Studies</Nav.Link>  {/* New Case Studies Tab */}
+            <Nav.Link as={Link} to="/my-portfolio/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
